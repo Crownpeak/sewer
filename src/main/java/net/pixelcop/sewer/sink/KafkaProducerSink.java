f563fe2b3131117c2ff2b5a0c8ecb05fdff36059
Anson Abraham <aabraham@ghostery.com>
2014-08-12 15:00:49 -0400

    reverted KafkaProducerSink to example.

--- src/main/java/net/pixelcop/sewer/sink/KafkaProducerSink.java
+++ src/main/java/net/pixelcop/sewer/sink/KafkaProducerSink.java
@@ -3,9 +3,7 @@ package net.pixelcop.sewer.sink;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.BinaryEncoder;
@@ -28,39 +26,29 @@ import kafka.producer.ProducerConfig;
 
 
 public class KafkaProducerSink {
-    private final Producer<String, Message> kafkaProducer;
-    private static final SpecificDatumWriter<kEvent> avroEventWriter = new SpecificDatumWriter<kEvent>(kEvent.SCHEMA$);
-    private static final EncoderFactory avroEncoderFactory = EncoderFactory.get();
 
+    public static void main(String[] args) {
+        long events = Long.parseLong(args[0]);
+        Random rnd = new Random();
 
-    public KafkaAvroPublisher(String settings) {
         Properties props = new Properties();
-//        props.put("broker.list", "kafka01:9092,kafka02:9092,kafka03:9092"); // if broker list is used
-        props.put(“zk.connect”, “127.0.0.1:2181,10.10.0.10:2181”)
-        props.put("serializer.class", "kafka.serializer.DefaultEncoder");
-        props.put("producer.type", "async");
+        props.put("metadata.broker.list", "broker1:9092,broker2:9092 ");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "example.producer.SimplePartitioner");
         props.put("request.required.acks", "1");
-        props.put("queue.enqueue.timeout.ms", "-1");
-        props.put("batch.num.messages", "300");
-        props.put("compression.codec", "snappy");
-        props.put("message.send.max.retries", "3");
-        kafkaProducer = new Producer<String, Message>(new ProducerConfig(props));
-        this.settings = settings;
-    }
 
+        ProducerConfig config = new ProducerConfig(props);
 
-    public void publish(kEvent event) {
-        try {
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            BinaryEncoder binaryEncoder = encoderFactory.binaryEncoder(stream, null);
-            avroEventWriter.write(event, binaryEncoder);
-            binaryEncoder.flush();
-            IOUtils.closeQuietly(stream);
+        Producer<String, String> producer = new Producer<String, String>(config);
 
-            Message m = new Message(stream.toByteArray());
-            producer.send(new ProducerRecord("rawdata", "key", "value")).get();
-        } catch (IOException e) {
-            throw new RuntimeException("Avro serialization failure", e);
+        for (long nEvents = 0; nEvents < events; nEvents++) {
+            long runtime = new Date().getTime();
+            String ip = "192.168.2." + rnd.nextInt(255);
+            String msg = runtime + ",www.example.com," + ip;
+            KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
+
         }
+        producer.close();
     }
+
 }
