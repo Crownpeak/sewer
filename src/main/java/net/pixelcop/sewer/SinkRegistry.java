3c51fea5a6a29488cca58523e6418822b4091264
Richard Craparotta <rcraparotta@evidon.com>
2014-09-10 12:29:28 -0400

    Moved RabbitMQ out of SequenceFile and no longer Extends it. I think SequenceFIle was overriding it when it tried to run.

--- src/main/java/net/pixelcop/sewer/SinkRegistry.java
+++ src/main/java/net/pixelcop/sewer/SinkRegistry.java
@@ -7,6 +7,7 @@ import net.pixelcop.sewer.sink.DfsSink;
 import net.pixelcop.sewer.sink.MeterSink;
 import net.pixelcop.sewer.sink.SequenceFileSink;
 import net.pixelcop.sewer.sink.RabbitMQSequenceSink;
+import net.pixelcop.sewer.sink.RabbitMQSink;
 import net.pixelcop.sewer.sink.TcpWriteableEventSink;
 import net.pixelcop.sewer.sink.buffer.AsyncBufferSink;
 import net.pixelcop.sewer.sink.buffer.BufferPoolSink;
@@ -31,6 +32,7 @@ public class SinkRegistry {
     register("dfs", DfsSink.class);
     register("seqfile", SequenceFileSink.class);
     register("rabbitseqfile", RabbitMQSequenceSink.class);
+    register("rabbitmq", RabbitMQSink.class);
     register("reliableseq", ReliableSequenceFileSink.class);
     register("tcpwrite", TcpWriteableEventSink.class);
 
---
+++ src/main/java/net/pixelcop/sewer/sink/RabbitMQSink.java
@@ -0,0 +1,102 @@
+package net.pixelcop.sewer.sink;
+
+import java.io.IOException;
+
+import net.pixelcop.sewer.DrainSink;
+import net.pixelcop.sewer.Event;
+import net.pixelcop.sewer.node.Node;
+import net.pixelcop.sewer.util.HdfsUtil;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.SequenceFile.CompressionType;
+import org.apache.hadoop.io.SequenceFile.Writer;
+import org.apache.hadoop.io.VLongWritable;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import RabbitMQ stuff needed to create and send information
+import net.pixelcop.sewer.SendRabbitMQTopic;
+//end of rabbitmq imports
+
+/**
+ *
+ * @author richard craparotta
+ *
+ */
+@DrainSink
+public class RabbitMQSink extends BucketedSink {
+
+  private static final Logger LOG = LoggerFactory.getLogger(RabbitMQSink.class);
+
+  private static final VLongWritable ONE = new VLongWritable(1L);
+
+   //RabbitMQ
+  SendRabbitMQTopic sendRabbit;
+  //end
+
+  public RabbitMQSink(String[] args) {
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::START RabbitMQSink Constructor");
+    }
+    //RabbitMQ
+    sendRabbit = new SendRabbitMQTopic();
+    //end
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::END RabbitMQSink Constructor");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::START RabbitMQSink close()");
+    }
+    //RabbitMQ
+    sendRabbit.close();
+    //end
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::END RabbitMQSink close()");
+    }
+  }
+
+  @Override
+  public void open() throws IOException {
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::START RabbitMQSink open()");
+    }
+    //RabbitMQ
+    sendRabbit.open();
+    //end
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::END RabbitMQSink open()");
+    }
+  }
+
+  @Override
+  public void append(Event event) throws IOException {
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::START RabbitMQSink append()");
+    }
+    //RabbitMQ
+    sendRabbit.sendMessage("This is a test message of routingKey of: l.ghostery.com");
+    //end
+
+    if (LOG.isInfoEnabled()) {
+      LOG.info(":::END RabbitMQSink append()");
+    }
+  }
+
+    @Override
+  public String generateNextBucket() {
+    return "";
+  }
+  
+  @Override
+  public String getFileExt() {
+    return "";
+  }
+
+}
