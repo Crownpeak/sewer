d504d0dcc98f3d09cd2c32b4cf8b307151b3d2bc
Richard Craparotta <rcraparotta@evidon.com>
2014-10-02 15:27:02 -0400

    added count to track messages in logs. Log messages get intermixed..

--- src/main/java/net/pixelcop/sewer/SendRabbitMQTopic.java
+++ src/main/java/net/pixelcop/sewer/SendRabbitMQTopic.java
@@ -72,16 +72,20 @@ public class SendRabbitMQTopic {
         factory.setVirtualHost(VIRTUAL_HOST);
     }
 
+    
+    int count = 1;
+    
     public void sendMessage(String message, String host) {    	
         if( host.equals(ROUTING_KEY)) {
             try{    
-            	LOG.info("TIMESTAMP: Sending Message");
+            	LOG.info("TIMESTAMP: Sending: "+count);
                 channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
                 if( CONFIRMS) {
-                    LOG.info("TIMESTAMP: WAITING FOR CONFIRMS");
+                    LOG.info("TIMESTAMP: Wainting: "+count);
                     boolean test = channel.waitForConfirms();
-                    LOG.info("TIMESTAMP: Done Sending Message. Exit");
+                    LOG.info("TIMESTAMP: Done. Exit: "+count);
 //                    LOG.info("RABBITMQ: Message ACKED? : "+test+"\n\t"+message);
+                    count++;
                 }
             } catch (IOException e) {
                 e.printStackTrace();
